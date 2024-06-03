094f6e4e5db073371cead4d64bb6ff83ec68e886
Amartin02 <Tankfurnace641@gmail.com>
2024-06-03 14:57:14 -0600

    more spelling issues

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -5,9 +5,9 @@ function Aboutme() {
     <div className="aboutMe-container">
       <p>
         My name is Anthony Martin. Here you can see my upcoming projects and my
-        contact information in case you'd like to work with me. In the past i
-        worked in HVAC for years but now i've decided in a career change. In
-        HVAC I quickly moved from an apprentice to lead installer where i
+        contact information in case you'd like to work with me. In the past I
+        worked in HVAC for years but now I've decided in a career change. In
+        HVAC I quickly moved from an apprentice to lead installer where I
         managed a crew of installers. I enjoy the backend and have learned alot
         during a three month coding bootcamp. Please see resume for further
         details. I plan to continue my education in this field and look forward
